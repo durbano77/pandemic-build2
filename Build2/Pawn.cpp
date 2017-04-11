83780bf143a39d8b7076e94f05fa85c4580477e1
Jasmine <jasmine.leblondc@gmail.com>
2017-04-11 13:53:56 -0400

    printPawn: playerName attribute added

--- Build2/Pawn.cpp
+++ Build2/Pawn.cpp
@@ -34,8 +34,8 @@ Pawn::~Pawn(){
 void Pawn::printPawn(){
     std::cout<<"----------------------------------------"<<std::endl;
     std::cout<<"Pawn: " << this->color <<std::endl;
+    std::cout<<"Player: " << this->playerName <<std::endl;
     std::cout<<"Location: "<<pawncity->getCityName()<<std::endl;
-    //std::cout<<getPlayerName()<<std::endl;
     std::cout<<"----------------------------------------\n\n"<<std::endl;
     
 }
